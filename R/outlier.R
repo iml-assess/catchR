cc33bc6f267f92f77609db4e375aca3c9ee2dd98
elisvb <elisvb@hotmail.com>
2021-01-29 14:49:49 -0500

    corrections
    - name Jordan
    - outlier function documentation

--- R/outlier.R
+++ R/outlier.R
@@ -1,6 +1,6 @@
 ##' Identify outliers based on the interquantile range
 ##' @param x vector
-##' @param coef 
+##' @param coef multiplication factor
 ##' @details Identifies outliers based on common rule of coef * the interquantile range. The default coefficient is set to 1.5. A coefficient of 3 is recommended for extremes.  Returns a vector of true or false.
 ##' @rdname outlier
 ##' @export
