46edfae2fed5984ddcc232f1bdb219d11f8371d0
elisvb <elisvb@hotmail.com>
2021-05-14 15:03:40 -0400

    add uncertainty

--- R/get.caa.R
+++ R/get.caa.R
@@ -3,7 +3,6 @@
 ##' @param plus Value of plus group (numeric). 
 ##' @details 
 #'  This function calculates catch-at-age number (caan), catch-at-age weight (caaw), mean weight-at-age (waa) and mean length-at-age (laa).
-#'  So-called scores were added as an exploratory way to gauge the quality of the estimates each years. 
 ##' @import dplyr tidyr 
 ##' @rdname get.caa
 ##' @export
@@ -11,17 +10,21 @@ get.caa <- function(x, plus = Inf){
     
     id.age <- grep('age\\.', colnames(x))
     bound <- function(x,y){x<- as.numeric(x);x[x>y]<-y;return(x)}
-
+    
     # calculations
     caa <- x %>%
-           pivot_longer(cols = all_of(id.age), names_to = "age", names_prefix = "age.", values_to = "age.prop") %>% # transform to long format
-           mutate(age = bound(age, plus), # make sure age is numeric and smaller than the plus group
-                  wt = catch * age.prop * lf.prop) %>%  # weight of fish of that length-age (caaw)
-           group_by(year,age) %>% 
-           summarise(caan = sum(wt/weight.unit),                 # caan according to Gavaris (BUT with weight by length)
-                     caaw = sum(wt),                              # caaw according to Gavaris (BUT with weight by length)
-                     laa = weighted.mean(length, wt/weight.unit), # weighted by their number at length and age in the catch
-                     waa = caaw / caan) %>%  
-           as.data.frame()
+        pivot_longer(cols = all_of(id.age), names_to = "age", names_prefix = "age.", values_to = "age.prop") %>% # transform to long format
+        mutate(age = bound(age, plus),            # make sure age is numeric and smaller than the plus group
+               wt = catch * age.prop * lf.prop,   # Ckij = Ck * pijk * pjk (in weight)
+               wtn = catch * age.prop * lf.prop / weight.unit) %>%  # Ckij = Ck * pijk * pjk / wjk (in numbers)
+        group_by(year,age) %>% 
+        summarise(caan = sum(wtn),                 
+                  caaw = sum(wt),                           
+                  waa = caaw / caan,      # idem to weighted.mean(weight.unit, wtn)
+                  waa.sd = sqrt(sum(wtn*(weight.unit-waa)^2)/(sum(wtn)-1)), # idem to Hmish package::wtd.var(weight.unit,wtn), see https://en.wikipedia.org/wiki/Weighted_arithmetic_mean 6.1 frequency weights (bessel's correction).  
+                  laa = weighted.mean(length, wtn),
+                  laa.sd = sqrt(sum(wtn*(length-laa)^2)/(sum(wtn)-1)))%>%   # idem to Hmish package::wtd.var(length,wtn), see https://en.wikipedia.org/wiki/Weighted_arithmetic_mean 6.1 frequency weights (bessel's correction). 
+        as.data.frame()
     return(caa)
 }
+
